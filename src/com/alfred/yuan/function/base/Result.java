40c47ba21f5f87b492705f5face47e952addb078
Alfred Yuan <yuanxun.cn@gmail.com>
2019-01-27 13:30:13 +0800

    the basic functional interface to enhance java function programming

---
+++ src/com/alfred/yuan/function/base/Result.java
@@ -0,0 +1,46 @@
+package com.alfred.yuan.function.base;
+
+/**
+ * Created by alfred_yuan on 2019-01-27
+ */
+public interface Result<T> {
+    void bind(Effect<T> success, Effect<String> failure);
+
+    static <T> Result<T> failure(String message) {
+        return new Failure<>(message);
+    }
+
+    static <T> Result<T> success(T value) {
+        return new Success<>(value);
+    }
+
+    class Success<T> implements Result<T> {
+        private final T value;
+
+        public Success(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public void bind(Effect<T> success, Effect<String> failure) {
+            success.apply(value);
+        }
+    }
+
+    class Failure<T> implements Result<T> {
+        private final String errorMessage;
+
+        public Failure(String s) {
+            this.errorMessage = s;
+        }
+
+        public String getMessage() {
+            return this.errorMessage;
+        }
+
+        @Override
+        public void bind(Effect<T> success, Effect<String> failure) {
+            failure.apply(errorMessage);
+        }
+    }
+}
